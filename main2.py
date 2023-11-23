feb2438026fa679fe5c1f212aa180fd831a17d96
draketss <110877231+draketss@users.noreply.github.com>
2023-11-22 18:56:21 -0600

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,9 @@
+from blackjack import JuegoBlackjack
+
+if __name__ == "__main__":
+    nombre_jugador1 = input("Ingresa el nombre del Jugador 1: ")
+    nombre_jugador2 = input("Ingresa el nombre del Jugador 2: ")
+    num_rondas = int(input("Ingresa el número de rondas a jugar: "))
+
+    juego_blackjack = JuegoBlackjack(nombre_jugador1, nombre_jugador2, num_rondas)
+    juego_blackjack.jugar()
